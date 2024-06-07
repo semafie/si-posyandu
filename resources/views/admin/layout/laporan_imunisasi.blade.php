5bf96172d1528c23811f92cf627762f7086614e6
semafie <restuimamsafii04@gmail.com>
2024-06-07 20:41:19 +0700

    perubahan target blank

--- resources/views/admin/layout/laporan_imunisasi.blade.php
+++ resources/views/admin/layout/laporan_imunisasi.blade.php
@@ -86,7 +86,7 @@
                 <h5 class="modal-title" id="modalToggleLabel">Pilih tanggal</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-              <form action="/cetakimunisasi/masuk" method="POST">
+              <form action="/cetakimunisasi/masuk" method="POST" target="_blank">
               @csrf
               <div class="modal-body">
                 <input class="form-control" name="tanggalprint" type="date"  id="html5-date-input" />
--- resources/views/admin/layout/laporan_pemeriksaan.blade.php
+++ resources/views/admin/layout/laporan_pemeriksaan.blade.php
@@ -90,7 +90,7 @@
                 <h5 class="modal-title" id="modalToggleLabel">Pilih tanggal</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-              <form action="/cetakpemeriksaan/masuk" method="POST">
+              <form action="/cetakpemeriksaan/masuk" method="POST" target="_blank">
               @csrf
               <div class="modal-body">
                 <input class="form-control" name="tanggalprint" type="date"  id="html5-date-input" />
